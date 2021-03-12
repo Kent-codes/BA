b506133632f3957caf44560793986796ce739e67
kento.okuda@asterone.co.jp <kento.okuda@asterone.co.jp>
2021-03-12 04:52:17 +0000

    tabs

---
+++ screens/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {
+
+    View,
+    Text,
+
+} from "react-native";
+
+
+const Home = () => {
+
+    return (
+        <View>
+            <Text> HOME </Text>
+        </View>
+    )
+}
+
+export default Home;
