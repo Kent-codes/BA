e6ffcba673bc86ccdfccebca09073351fd97dc70
kento.okuda@asterone.co.jp <kento.okuda@asterone.co.jp>
2021-03-12 09:28:05 +0000

    Navigation

--- App.js
+++ App.js
@@ -1,27 +1,20 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { Home, Restaurant, OrderDelivery } from './screens';
 import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from "@react-navigation/native";
+import Tabs from './navigation/tabs'
+import { Home, Restaurant, OrderDelivery } from './screens';
 
 const Stack = createStackNavigator();
 
-
-export default function App() {
+function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigater
-        screenOptions={{
-          headerShown: false
-        }}
-        initialRouteName={"Home"}
-      >
-        <Stack.Screen name="Home" Component={Home} />
-        <Stack.Screen name="Restaurant" Component={Restaurant} />
-        <Stack.Screen name="OrderDelivery" Component={OrderDelivery} />
-      </Stack.Navigater>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Tabs} />
+        <Stack.Screen name="Restaurant" component={Restaurant} />
+        <Stack.Screen name="OrderDelivery" component={OrderDelivery} />
+      </Stack.Navigator>
     </NavigationContainer>
-
-  )
+  );
 }
-
-
+export default App;
