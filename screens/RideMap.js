ece36aa49090d875da7624a36fdc124cad651bb4
KENTO OKUDA <okendy212@gmail.com>
2021-03-30 13:01:43 +0900

    ボタン微調整

--- screens/RideMap.js
+++ screens/RideMap.js
@@ -232,48 +232,48 @@ const RideMap = ({ route, navigation }) => {
         )
     }
 
-    function renderDestinationHeader() {
-        return (
-            <View
-                style={{
-                    position: 'absolute',
-                    top: 50,
-                    left: 0,
-                    right: 0,
-                    height: 50,
-                    alignItems: 'center',
-                    justifyContent: 'center'
-                }}
-            >
-                <View
-                    style={{
-                        flexDirection: 'row',
-                        alignItems: 'center',
-                        width: SIZES.width * 0.9,
-                        paddingVertical: SIZES.padding,
-                        paddingHorizontal: SIZES.padding * 2,
-                        borderRadius: SIZES.radius,
-                        backgroundColor: COLORS.white
-                    }}
-                >
-                    <Image
-                        source={icons.red_pin}
-                        style={{
-                            width: 30,
-                            height: 30,
-                            marginRight: SIZES.padding
-                        }}
-                    />
+    // function renderDestinationHeader() {
+    //     return (
+    //         <View
+    //             style={{
+    //                 position: 'absolute',
+    //                 top: 50,
+    //                 left: 0,
+    //                 right: 0,
+    //                 height: 50,
+    //                 alignItems: 'center',
+    //                 justifyContent: 'center'
+    //             }}
+    //         >
+    //             <View
+    //                 style={{
+    //                     flexDirection: 'row',
+    //                     alignItems: 'center',
+    //                     width: SIZES.width * 0.9,
+    //                     paddingVertical: SIZES.padding,
+    //                     paddingHorizontal: SIZES.padding * 2,
+    //                     borderRadius: SIZES.radius,
+    //                     backgroundColor: COLORS.white
+    //                 }}
+    //             >
+    //                 <Image
+    //                     source={icons.red_pin}
+    //                     style={{
+    //                         width: 30,
+    //                         height: 30,
+    //                         marginRight: SIZES.padding
+    //                     }}
+    //                 />
 
-                    <View style={{ flex: 1 }}>
-                        <Text style={{ ...FONTS.body3 }}>{streetName}</Text>
-                    </View>
+    //                 <View style={{ flex: 1 }}>
+    //                     <Text style={{ ...FONTS.body3 }}>{streetName}</Text>
+    //                 </View>
 
-                    <Text style={{ ...FONTS.body3 }}>{Math.ceil(duration)} mins</Text>
-                </View>
-            </View>
-        )
-    }
+    //                 <Text style={{ ...FONTS.body3 }}>{Math.ceil(duration)} mins</Text>
+    //             </View>
+    //         </View>
+    //     )
+    // }
 
     function renderDeliveryInfo(selectedId) {
 
@@ -284,7 +284,7 @@ const RideMap = ({ route, navigation }) => {
             <View
                 style={{
                     position: 'absolute',
-                    bottom: 50,
+                    bottom: 85,
                     left: 0,
                     right: 0,
                     alignItems: 'center',
@@ -381,8 +381,8 @@ const RideMap = ({ route, navigation }) => {
                     position: 'absolute',
                     top: SIZES.height * 0.01,
                     // right: SIZES.padding * 0.2,
-                    // left: SIZES.padding * 0.2,
-                    width: SIZES.width,
+                    left: SIZES.padding * 1.7,
+                    width: SIZES.width * 0.9,
                     height: 70,
                     flexDirection: 'row',
                     // alignItems: 'baseline',
